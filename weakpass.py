837265c0e3743f1e191354fdcea1570dba0d0432
sunw4r <sunwar@gmail.com>
2022-02-14 11:12:10 -0300

    Some changes to make modifications easier

--- weakpass.py
+++ weakpass.py
@@ -3,10 +3,18 @@
 import sys
 from datetime import datetime
 
+# target variations
 company = str(sys.argv[1]).lower()
 companyCap = company.capitalize()
 companyFullCap = company.upper()
 
+caseTypes = [
+    company, 
+    companyCap, 
+    companyFullCap
+]
+
+# numbers variations
 currentYear = str(datetime.now().year)
 currentYearM = str(datetime.now().year - 1)
 currentYearMM = str(datetime.now().year - 2)
@@ -15,239 +23,49 @@ currentYearMMMM = str(datetime.now().year - 4)
 currentYearMMMMM = str(datetime.now().year - 5)
 currentYearP = str(datetime.now().year + 1)
 
-#Cap
+numbers = [
+    currentYear,
+    currentYearM,
+    currentYearMM,
+    currentYearMMM,
+    currentYearMMMM,
+    currentYearMMMMM,
+    currentYearP,
+    "1",
+    "12",
+    "123",
+    "1234",
+    "12345"
+]
 
-print(companyCap+"@"+str(currentYear))
-print(companyCap+"@"+str(currentYearM))
-print(companyCap+"@"+str(currentYearMM))
-print(companyCap+"@"+str(currentYearMMM))
-print(companyCap+"@"+str(currentYearMMMM))
-print(companyCap+"@"+str(currentYearMMMMM))
-print(companyCap+"@1")
-print(companyCap+"@12")
-print(companyCap+"@123")
-print(companyCap+"@1234")
-print(companyCap+"@12345")
-print(companyCap+"@"+str(currentYearP))
+# special chars
+specials = [
+    "@",
+    "",
+    "!",
+    "*",
+    "#"
+]
 
-print(companyCap+""+str(currentYear))
-print(companyCap+""+str(currentYearM))
-print(companyCap+""+str(currentYearMM))
-print(companyCap+""+str(currentYearMMM))
-print(companyCap+""+str(currentYearMMMM))
-print(companyCap+""+str(currentYearMMMMM))
-print(companyCap+"1")
-print(companyCap+"12")
-print(companyCap+"123")
-print(companyCap+"1234")
-print(companyCap+"12345")
-print(companyCap+""+str(currentYearP))
+# to remove duplicates
+wpwdlist = list()
 
-print("@"+companyCap+str(currentYear))
-print("@"+companyCap+str(currentYearM))
-print("@"+companyCap+str(currentYearMM))
-print("@"+companyCap+str(currentYearMMM))
-print("@"+companyCap+str(currentYearMMMM))
-print("@"+companyCap+str(currentYearMMMMM))
-print("@1"+companyCap)
-print("@12"+companyCap)
-print("@123"+companyCap)
-print("@1234"+companyCap)
-print("@12345"+companyCap)
-print("@"+companyCap+str(currentYearP))
+# let's mix
+for x in caseTypes:
+    for y in numbers:
+        for z in specials:
+            wpwdlist.append(x+z+y)
+            wpwdlist.append(x+y+z)
 
-print(companyCap+""+str(currentYear)+"!")
-print(companyCap+""+str(currentYearM)+"!")
-print(companyCap+""+str(currentYearMM)+"!")
-print(companyCap+""+str(currentYearMMM)+"!")
-print(companyCap+""+str(currentYearMMMM)+"!")
-print(companyCap+""+str(currentYearMMMMM)+"!")
-print(companyCap+"1"+"!")
-print(companyCap+"12"+"!")
-print(companyCap+"123"+"!")
-print(companyCap+"1234"+"!")
-print(companyCap+"12345"+"!")
-print(companyCap+""+str(currentYearP)+"!")
+output = list(dict.fromkeys(wpwdlist))
 
-print(companyCap+"!"+str(currentYear))
-print(companyCap+"!"+str(currentYearM))
-print(companyCap+"!"+str(currentYearMM))
-print(companyCap+"!"+str(currentYearMMM))
-print(companyCap+"!"+str(currentYearMMMM))
-print(companyCap+"!"+str(currentYearMMMMM))
-print(companyCap+"!1")
-print(companyCap+"!12")
-print(companyCap+"!123")
-print(companyCap+"!1234")
-print(companyCap+"!12345")
-print(companyCap+"!"+str(currentYearP))
-
-print(companyCap+"*"+str(currentYear))
-print(companyCap+"*"+str(currentYearM))
-print(companyCap+"*"+str(currentYearMM))
-print(companyCap+"*"+str(currentYearMMM))
-print(companyCap+"*"+str(currentYearMMMM))
-print(companyCap+"*"+str(currentYearMMMMM))
-print(companyCap+"*1")
-print(companyCap+"*12")
-print(companyCap+"*123")
-print(companyCap+"*1234")
-print(companyCap+"*12345")
-print(companyCap+"*"+str(currentYearP))
-
-#lowercase
-
-print(company+"@"+str(currentYear))
-print(company+"@"+str(currentYearM))
-print(company+"@"+str(currentYearMM))
-print(company+"@"+str(currentYearMMM))
-print(company+"@"+str(currentYearMMMM))
-print(company+"@"+str(currentYearMMMMM))
-print(company+"@1")
-print(company+"@12")
-print(company+"@123")
-print(company+"@1234")
-print(company+"@12345")
-print(company+"@"+str(currentYearP))
-
-print("@"+company+str(currentYear))
-print("@"+company+str(currentYearM))
-print("@"+company+str(currentYearMM))
-print("@"+company+str(currentYearMMM))
-print("@"+company+str(currentYearMMMM))
-print("@"+company+str(currentYearMMMMM))
-print("@1"+company)
-print("@12"+company)
-print("@123"+company)
-print("@1234"+company)
-print("@12345"+company)
-print("@"+company+str(currentYearP))
-
-print(company+""+str(currentYear))
-print(company+""+str(currentYearM))
-print(company+""+str(currentYearMM))
-print(company+""+str(currentYearMMM))
-print(company+""+str(currentYearMMMM))
-print(company+""+str(currentYearMMMMM))
-print(company+"1")
-print(company+"12")
-print(company+"123")
-print(company+"1234")
-print(company+"12345")
-print(company+""+str(currentYearP))
-
-print(company+""+str(currentYear)+"!")
-print(company+""+str(currentYearM)+"!")
-print(company+""+str(currentYearMM)+"!")
-print(company+""+str(currentYearMMM)+"!")
-print(company+""+str(currentYearMMMM)+"!")
-print(company+""+str(currentYearMMMMM)+"!")
-print(company+"1"+"!")
-print(company+"12"+"!")
-print(company+"123"+"!")
-print(company+"1234"+"!")
-print(company+"12345"+"!")
-print(company+""+str(currentYearP)+"!")
-
-print(company+"!"+str(currentYear))
-print(company+"!"+str(currentYearM))
-print(company+"!"+str(currentYearMM))
-print(company+"!"+str(currentYearMMM))
-print(company+"!"+str(currentYearMMMM))
-print(company+"!"+str(currentYearMMMMM))
-print(company+"!1")
-print(company+"!12")
-print(company+"!123")
-print(company+"!1234")
-print(company+"!12345")
-print(company+"!"+str(currentYearP))
-
-print(company+"*"+str(currentYear))
-print(company+"*"+str(currentYearM))
-print(company+"*"+str(currentYearMM))
-print(company+"*"+str(currentYearMMM))
-print(company+"*"+str(currentYearMMMM))
-print(company+"*"+str(currentYearMMMMM))
-print(company+"*1")
-print(company+"*12")
-print(company+"*123")
-print(company+"*1234")
-print(company+"*12345")
-print(company+"*"+str(currentYearP))
-
-#fulluppercase
-
-print(companyFullCap+"@"+str(currentYear))
-print(companyFullCap+"@"+str(currentYearM))
-print(companyFullCap+"@"+str(currentYearMM))
-print(companyFullCap+"@"+str(currentYearMMM))
-print(companyFullCap+"@"+str(currentYearMMMM))
-print(companyFullCap+"@"+str(currentYearMMMMM))
-print(companyFullCap+"@1")
-print(companyFullCap+"@12")
-print(companyFullCap+"@123")
-print(companyFullCap+"@1234")
-print(companyFullCap+"@12345")
-print(companyFullCap+"@"+str(currentYearP))
-
-print(companyFullCap+""+str(currentYear))
-print(companyFullCap+""+str(currentYearM))
-print(companyFullCap+""+str(currentYearMM))
-print(companyFullCap+""+str(currentYearMMM))
-print(companyFullCap+""+str(currentYearMMMM))
-print(companyFullCap+""+str(currentYearMMMMM))
-print(companyFullCap+"1")
-print(companyFullCap+"12")
-print(companyFullCap+"123")
-print(companyFullCap+"1234")
-print(companyFullCap+"12345")
-print(companyFullCap+""+str(currentYearP))
-
-print(companyFullCap+""+str(currentYear)+"!")
-print(companyFullCap+""+str(currentYearM)+"!")
-print(companyFullCap+""+str(currentYearMM)+"!")
-print(companyFullCap+""+str(currentYearMMM)+"!")
-print(companyFullCap+""+str(currentYearMMMM)+"!")
-print(companyFullCap+""+str(currentYearMMMMM)+"!")
-print(companyFullCap+"1"+"!")
-print(companyFullCap+"12"+"!")
-print(companyFullCap+"123"+"!")
-print(companyFullCap+"1234"+"!")
-print(companyFullCap+"12345"+"!")
-print(companyFullCap+""+str(currentYearP)+"!")
-
-print(companyFullCap+"!"+str(currentYear))
-print(companyFullCap+"!"+str(currentYearM))
-print(companyFullCap+"!"+str(currentYearMM))
-print(companyFullCap+"!"+str(currentYearMMM))
-print(companyFullCap+"!"+str(currentYearMMMM))
-print(companyFullCap+"!"+str(currentYearMMMMM))
-print(companyFullCap+"!1")
-print(companyFullCap+"!12")
-print(companyFullCap+"!123")
-print(companyFullCap+"!1234")
-print(companyFullCap+"!12345")
-print(companyFullCap+"!"+str(currentYearP))
-
-print(companyFullCap+"*"+str(currentYear))
-print(companyFullCap+"*"+str(currentYearM))
-print(companyFullCap+"*"+str(currentYearMM))
-print(companyFullCap+"*"+str(currentYearMMM))
-print(companyFullCap+"*"+str(currentYearMMMM))
-print(companyFullCap+"*"+str(currentYearMMMMM))
-print(companyFullCap+"*1")
-print(companyFullCap+"*12")
-print(companyFullCap+"*123")
-print(companyFullCap+"*1234")
-print(companyFullCap+"*12345")
-print(companyFullCap+"*"+str(currentYearP))
+for wpwd in output:
+    print(wpwd);
 
 #generics
-
 print("Sucesso1!")
 print("Sucesso10!")
 print("Mudar@123")
 print("Mudar123")
 print("mudar@123")
 print("mudar123")
-
